1eb0adf2e3bf20737adaefa2cebb4fdc69199957
Blas Medina <blasmedina5@gmail.com>
2020-11-06 16:31:16 -0300

    1.9  devices time regulation

--- game.js
+++ game.js
@@ -252,7 +252,7 @@
     }
 
     function run() {
-        window.requestAnimationFrame(run);
+        setTimeout(run, 50);
     
         var now = Date.now(),
             deltaTime = (now - lastUpdate) / 1000;
